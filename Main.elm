3b51e1915a30d1ebf56d2bea75b08e8eabaaf6db
Felipe Apostol <flipjs.io@gmail.com>
2016-10-22 22:32:38 +0100

    initial commit

---
+++ Main.elm
@@ -0,0 +1,296 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.App as App
+import Html.Events exposing (..)
+import Html.Attributes exposing (..)
+
+
+main : Program Never
+main =
+    App.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+type alias Model =
+    { players : List Player
+    , plays : List Play
+    , input : String
+    , playerId : Maybe Int
+    , totalScore : Int
+    }
+
+
+type alias Player =
+    { id : Int
+    , name : String
+    , score : Int
+    }
+
+
+type alias Play =
+    { id : Int
+    , playerName : String
+    , playerId : Int
+    , score : Int
+    }
+
+
+type Msg
+    = Input String
+    | EditPlayer Player
+    | Save
+    | Cancel
+    | Score Player Int
+    | DeletePlay Play
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Input name ->
+            ( { model
+                | input = name
+              }
+            , Cmd.none
+            )
+
+        Cancel ->
+            ( { model
+                | input = ""
+                , playerId = Nothing
+              }
+            , Cmd.none
+            )
+
+        Save ->
+            case model.playerId of
+                Just id ->
+                    if model.input == "" then
+                        ( { model
+                            | input = ""
+                            , playerId = Nothing
+                          }
+                        , Cmd.none
+                        )
+                    else
+                        updatePlayer model id
+
+                Nothing ->
+                    if model.input == "" then
+                        ( model, Cmd.none )
+                    else
+                        savePlayer model
+
+        EditPlayer player ->
+            ( { model
+                | input = player.name
+                , playerId = Just player.id
+              }
+            , Cmd.none
+            )
+
+        Score player score ->
+            let
+                players =
+                    model.players
+                        |> List.map
+                            (\p ->
+                                if p.id == player.id then
+                                    { p | score = p.score + score }
+                                else
+                                    p
+                            )
+
+                play =
+                    Play
+                        (List.length model.plays)
+                        player.name
+                        player.id
+                        score
+
+                plays =
+                    play :: model.plays
+
+                totalScore =
+                    model.totalScore + score
+            in
+                ( { model
+                    | players = players
+                    , plays = plays
+                    , totalScore = totalScore
+                  }
+                , Cmd.none
+                )
+
+        DeletePlay play ->
+            let
+                plays =
+                    model.plays
+                        |> List.filter (\p -> p.id /= play.id)
+
+                players =
+                    model.players
+                        |> List.map
+                            (\p ->
+                                if p.id == play.playerId then
+                                    { p | score = p.score - play.score }
+                                else
+                                    p
+                            )
+
+                totalScore =
+                    model.totalScore - play.score
+            in
+                ( { model
+                    | players = players
+                    , plays = plays
+                    , totalScore = totalScore
+                  }
+                , Cmd.none
+                )
+
+
+savePlayer : Model -> ( Model, Cmd Msg )
+savePlayer model =
+    let
+        player =
+            Player (List.length model.players) model.input 0
+
+        players =
+            player :: model.players
+    in
+        ( { model
+            | players = players
+            , input = ""
+            , playerId = Nothing
+          }
+        , Cmd.none
+        )
+
+
+updatePlayer : Model -> Int -> ( Model, Cmd Msg )
+updatePlayer model id =
+    let
+        players =
+            model.players
+                |> List.map
+                    (\p ->
+                        if p.id == id then
+                            { p | name = model.input }
+                        else
+                            p
+                    )
+
+        plays =
+            model.plays
+                |> List.map
+                    (\p ->
+                        if p.playerId == id then
+                            { p | playerName = model.input }
+                        else
+                            p
+                    )
+    in
+        ( { model
+            | players = players
+            , plays = plays
+            , input = ""
+            , playerId = Nothing
+          }
+        , Cmd.none
+        )
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model [] [] "" Nothing 0, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ pageTitle
+        , playerForm model
+        , playersHeader
+        , playerList model.players
+        , playsHeader
+        , playList model.plays
+        , totalScore model
+        ]
+
+
+playerList : List Player -> Html Msg
+playerList players =
+    div []
+        [ players |> List.sortBy .name |> List.map playerItem |> ul [] ]
+
+
+playerItem : Player -> Html Msg
+playerItem player =
+    li []
+        [ text player.name
+        , player.score |> toString |> (++) " " |> text
+        , text " points "
+        , button [ onClick (Score player 2) ] [ text "score 2 points" ]
+        , button [ onClick (Score player 3) ] [ text "score 3 points" ]
+        , button [ onClick (EditPlayer player) ] [ text "Edit Player" ]
+        ]
+
+
+playList : List Play -> Html Msg
+playList plays =
+    div []
+        [ plays |> List.map playItem |> ul [] ]
+
+
+playItem : Play -> Html Msg
+playItem play =
+    li []
+        [ text (play.playerName ++ " scored " ++ (toString play.score))
+        , button [ onClick (DeletePlay play) ] [ text "Delete Play" ]
+        ]
+
+
+totalScore : Model -> Html Msg
+totalScore model =
+    h3 [] [ text ("Total score: " ++ (toString model.totalScore)) ]
+
+
+pageTitle : Html Msg
+pageTitle =
+    h1 [] [ text "Scorekeeper" ]
+
+
+playersHeader : Html Msg
+playersHeader =
+    h3 [] [ text "Players:" ]
+
+
+playsHeader : Html Msg
+playsHeader =
+    h3 [] [ text "Plays:" ]
+
+
+playerForm : Model -> Html Msg
+playerForm model =
+    Html.form [ onSubmit Save ]
+        [ input
+            [ type' "text"
+            , placeholder "Add/Edit Player..."
+            , value model.input
+            , onInput Input
+            ]
+            []
+        , button [ type' "submit" ] [ text "Save" ]
+        , button [ type' "button", onClick Cancel ] [ text "Cancel" ]
+        ]
---
+++ README.md
@@ -0,0 +1,6 @@
+# ScoreKeeper example using Elm
+
+Scorekeeper example used in http://courses.knowthen.com with my own implementation using Elm Architecture pattern.
+
+Run `elm-reactor --port=3000`,
+then go to http://localhost:3000
